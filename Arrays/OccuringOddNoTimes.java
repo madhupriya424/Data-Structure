a359e4c7c4139bcda0603b1d50cdafdcca5dd980
madhupriya424 <madhupriya424@gmail.com>
2017-06-14 11:38:04 +0530

    Create OccuringOddNoTimes.java

--- Arrays/OccuringOddNoTimes.java
+++ Arrays/OccuringOddNoTimes.java
@@ -1,3 +1,7 @@
+//BY USING HASHMAP
+//TIME COMPLEXITY: O(n)
+//SPACE COMPLEXITY: O(n)
+
 import java.io.*;
 import java.util.*;
 
@@ -19,7 +23,8 @@ class GFG {
 	    }
 	    
 
-//	    for(Map.Entry<Integer, Integer> entry : hm.entrySet())
+//  for(Map.Entry<Integer, Integer> entry : hm.entrySet())
+//  we can also write it as Line 28:		
         for(int i=0;i<a.length;i++)
 	    {
 	        if(hm.get(a[i])%2!=0)
@@ -30,3 +35,28 @@ class GFG {
 	    }
      }
 }     
+
+
+// Method 2 : More efficient than above code
+//  XOR Operation
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+		int arr[]={1, 2, 3, 2, 3, 1, 3};
+		OccuringOddNo(arr);
+	}
+	
+	static void OccuringOddNo(int a[])
+	{
+	   int res=0;
+	   for(int i=0;i<a.length;i++)
+	       res = res^a[i];         // Performing Xor Operation
+     System.out.print(res);
+    }     
+}
+
+//TIME COMPLEXITY: O(n)
+//SPACE COMPLEXITY: O(1)
