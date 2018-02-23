1be58773356ed6f335781b0b993122635d44e47f
Madhu Priya <madhupriya424@gmail.com>
2018-02-23 23:07:27 +0530

    Update GraphMatrixBFS.java

--- Graph/GraphMatrixBFS.java
+++ Graph/GraphMatrixBFS.java
@@ -1,48 +1,66 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
 import java.util.*;
 
-class graphMatixBFS {
+class GraphMatrixBfs {
     private int v;
     private int adj[][];
     
-    graphMatixBFS(int v){
-        this.v=v;
+    GraphMatrixBfs(int v){
+        this.v = v;
         adj = new int[v][v];
     }
     
     void addEdge(int u, int v){
         adj[u][v]=1;
+        adj[v][u]=1;
     }
     
-    
-    void bfs(int source){
+    void bfs(int s){
         boolean visited[] = new boolean[v];
+     
         Queue<Integer> q = new LinkedList<Integer>();
-        q.add(source);
-        visited[source]=true;
+        q.add(s);
+        visited[s]=true;
         
         while(!q.isEmpty())
         {
-            int currrNode = q.remove();
-            System.out.print(currrNode+" ");
-            
-            for(int j=0;j<v;j++){
-                if(visited[j]==false && adj[currrNode][j]==1){
-                    q.add(j);
-                    visited[j]=true;
-                }
-            }   
+         s = q.remove();
+        System.out.print(s+" ");
+        
+         for(int i=0;i<v;i++)
+         {
+            if(adj[s][i]==1 && !visited[i])
+            {
+                visited[i]=true;
+                q.add(i);
+            }
+              
+          }
         }
     }
-    
-    public static void main(String args[]){
-        graphMatixBFS g = new graphMatixBFS(7);
+    /*
+    0----------1\
+    |        / | \
+    |       /  |  \
+    |      /   |   2
+    |     /    |  /
+    |    /     | /
+    4----------3/
+    */
+	public static void main (String[] args) {
+	    GraphMatrixBfs g = new GraphMatrixBfs(7);
 	    g.addEdge(0,1);
-	    g.addEdge(1,5);
+	    g.addEdge(0,4);
 	    g.addEdge(1,2);
-	    g.addEdge(2,4);
-	    g.addEdge(4,3);
-	    g.addEdge(5,6);
-	    
+	    g.addEdge(1,3);
+	    g.addEdge(1,4);
+	    g.addEdge(2,3);
+	    g.addEdge(3,4);
+	
 	    g.bfs(0);
-    }
-}   
+	}
+}
+
+# 0 1 4 2 3
