3f3d83a21c71accd88a7975926a1c23692fb1c2e
Madhu Priya <madhupriya424@gmail.com>
2018-05-29 20:52:01 +0530

    Update TopView.java

--- Binary Tree/TopView.java
+++ Binary Tree/TopView.java
@@ -1,63 +1,76 @@
+/*package whatever //do not write package name here */
+
 import java.io.*;
 import java.util.*;
 
-class Node{
-    int data;
-    Node left;
-    Node right;
+class Node<T>{
+    T data;
+    Node<T> left;
+    Node<T> right;
     
-    Node(int data){
+    Node(T data){
         this.data = data;
     }
 }
 
-
 class BinaryTree {
     
-    Node root;
-    
-    static class QueueItem{
-       Node node ;
-       int horizontal_Distance;
-    
-    QueueItem(Node node, int horizontal_Distance){
-        this.node = node;
-        this.horizontal_Distance = horizontal_Distance;
-    }
-    }
-    
-    static TreeMap<Integer, Integer> tm = new TreeMap<>(); 
-    
-    
-    public static void TopView(Node root){
+    static class QueItem{
+        Node<Integer> node;
+        int horizontal_distance;
         
+        QueItem(Node<Integer> node, int horizontal_distance){    //?
+            this.node=node;
+            this.horizontal_distance=horizontal_distance;
+        }
+    }    
+    
+    static Node root;
+    static TreeMap<Integer, Integer> tm = new TreeMap<>();
+    
+    public static void topView(Node root){
         if(root==null)
-           return;
-           
-        Queue<QueueItem> q = new LinkedList<QueueItem>();
-        q.add(new QueueItem(root, 0));
+            return;
         
-        while(!q.isEmpty()) {
-            QueueItem qItem = q.remove();
-            Node currNode = qItem.node;
-            int hd = qItem.horizontal_Distance;
+        Queue<QueItem> q = new LinkedList<QueItem>();
+        q.add(new QueItem(root, 0));
+        
+        
+        while(!q.isEmpty())
+        {
+            QueItem qitem = q.remove();
+            Node currNode = qitem.node;
+            int hd = qitem.horizontal_distance;
             
-            if(!tm.containsKey(hd)){
-                tm.put(hd, currNode.data);
-            }
-            
-            if(currNode.left!=null){
-                q.add(new QueueItem(currNode.left, hd-1));
-            }
-            
-            if(currNode.right!=null){
-                q.add(new QueueItem(currNode.right, hd+1));
-            }
-        }    
+            if(!tm.containsKey(hd))
+                tm.put(hd, (Integer)currNode.data);
+                
+            if(currNode.left!=null)
+                q.add(new QueItem(currNode.left, hd-1));
+                
+            if(currNode.right!=null)
+                q.add(new QueItem(currNode.right, hd+1));
+        }
     }
-        
     
-
+    
+    
+	public static void main (String[] args) {
+	     BinaryTree tree = new BinaryTree();
+        tree.root = new Node<Integer>(1);
+        tree.root.left = new Node<Integer>(2);
+        tree.root.right = new Node<Integer>(3);
+        tree.root.left.left = new Node<Integer>(4);
+        tree.root.right.left = new Node<Integer>(5);
+	    tree.root.right.right = new Node<Integer>(6);
+	    tree.root.right.right.right = new Node<Integer>(7);
+	     tree.root.right.right.right.right = new Node<Integer>(8);
+	     topView(tree.root);
+	     
+	    for(Map.Entry<Integer, Integer> t: tm.entrySet())
+	        System.out.print(t.getValue()+" ");
+	}
+}
     
     // 1 2 3 4 -1 5 6 -1 -1 -1 -1 -1 7 -1 8 -1 -1
     /*                    1
@@ -69,25 +82,5 @@ class BinaryTree {
                                 7
                                  \
                                   8
-    */
-	public static void main (String[] args) {
-	    
-	    BinaryTree tree = new BinaryTree();
-        tree.root = new Node(1);
-        tree.root.left = new Node(2);
-        tree.root.right = new Node(3);
-        tree.root.left.left = new Node(4);
-        tree.root.right.left = new Node(5);
-	    tree.root.right.right = new Node(6);
-	    tree.root.right.right.right = new Node(7);
-	     tree.root.right.right.right.right = new Node(8);
-	     TopView(tree.root);
-	     
-	     for(Map.Entry<Integer, Integer> entry: tm.entrySet()){
-	         System.out.print(entry.getValue()+" ");
-	     }
-		//System.out.println(res);
-	}
-}
 
 # 4 2 1 3 6 7 8 
