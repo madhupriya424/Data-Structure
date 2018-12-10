7046d41e887e01fbafbffa069054fb03b87d78e7
Madhu Priya <madhupriya424@gmail.com>
2018-12-10 19:06:10 +0530

    Create numIslands.java

---
+++ Graph/numIslands.java
@@ -0,0 +1,68 @@
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+Input:
+11110
+11010
+11000
+00000
+
+Output: 1
+Example 2:
+
+Input:
+11000
+11000
+00100
+00011
+
+Output: 3
+
+
+----------------------------------------------------------------------------------------------------------------------------
+class Solution {
+    public int numIslands(char[][] grid) {
+        if(grid.length==0)
+            return 0;
+        
+        boolean visited[][]= new  boolean[grid.length][grid[0].length];
+        
+        int count=0;
+        
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            {
+                if(grid[i][j]=='1' && !visited[i][j]){
+                    numIslands(i, j, grid, visited);
+                        count++;
+                }    
+            }    
+        }
+        return count;
+    }
+        
+         public boolean isSafe(int i, int j, char[][] grid){
+             
+             if(i<0 || i>=grid.length ||  j<0 || j>=grid[0].length)
+                return false;
+             return true;
+         }
+        
+         public void numIslands(int i, int j, char grid[][], boolean visited[][]){
+             visited[i][j]=true;
+              
+         if(isSafe(i-1, j, grid) && !visited[i-1][j] && grid[i-1][j] == '1')
+            numIslands(i-1, j, grid, visited);
+            
+         if(isSafe(i, j-1, grid) && !visited[i][j-1] && grid[i][j-1] == '1')
+            numIslands(i, j-1, grid, visited);
+            
+         if(isSafe(i+1, j, grid) && !visited[i+1][j] && grid[i+1][j] == '1') 
+            numIslands(i+1, j, grid, visited);
+            
+         if(isSafe(i, j+1, grid) && !visited[i][j+1] && grid[i][j+1] == '1') 
+            numIslands(i, j+1, grid, visited);   
+         }   
+    }
