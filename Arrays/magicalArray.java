86d75c379ec430d66a8d5efc59a94cdce7ea06b9
Madhu Priya <madhupriya424@gmail.com>
2018-12-17 08:00:19 +0000

    Update magicalArray.java

--- Arrays/magicalArray.java
+++ Arrays/magicalArray.java
@@ -1,3 +1,71 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+class GFG {
+    
+    public static boolean isPrime(int n){
+        for(int i=2;i<n;i++){
+            if(i%n==0)
+                return false;
+        }
+        return true;;
+    }
+    
+    public static void binarySearch(int prime[], int a[], int k){
+        
+        for(int i=0;i<a.length;i++)
+        {
+            int target = a[i];
+            int start = 0;
+            int end = prime.length-1;
+            int j = 0;
+            while(start<end)
+            {
+                int mid = (start+end)/2;
+                if(prime[mid]==tartget){
+                  res[j]=target-prime[mid];
+                  j++;
+                  break;
+                }
+                else if(mid!=0 && target>prime[mid-1] && target<prime[mid]){
+                    res[j] = target- a[mid-1] < a[mid]-target ? (target- a[mid-1]) : (a[mid]-target);
+                }
+                else if(mid==0)
+            }
+        }
+    }
+    
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int a[] = sc.nextInt();
+		int max=0;
+		for(int i=0;i<N;i++){
+		    a[i] = sc.nextInt();
+		    int max1 = a[i];
+		    if(max<max1)
+		        max = a[i];
+		}
+		
+		int prime[] = new int[max];
+		for(int i=2;i<=max;i++){
+		    if(isPrime(i)){
+		     prime[j]=i*i;
+		}
+		}
+		
+		Arrays.sort(prime);
+		
+		binarySearch(prime, a, K);
+		
+		//System.out.println("GfG!");
+	}
+}
+
+
+
 // Java program to find GCD of two or 
 // more numbers 
 import java.util.*;
