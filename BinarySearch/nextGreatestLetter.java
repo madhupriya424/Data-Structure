17efd0f9be744b4679f7f0e442f8cf4097f4351b
Madhu Priya <madhupriya424@gmail.com>
2018-12-19 01:22:39 +0530

    Create nextGreatestLetter.java

---
+++ BinarySearch/nextGreatestLetter.java
@@ -0,0 +1,54 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        
+        int start = 0;
+        int end = letters.length-1;
+        if(target >= letters[end])
+            return letters[start];
+        while(start<=end)
+        {
+            int mid = start + (end-start)/2;
+            if(letters[mid]>target)
+                end = mid-1; 
+            else
+                start = mid+1;
+        }
+        return letters[start];
+    }
+}
+----------------------------------------------------------------------------------------------------------------------------
+
+Given a list of sorted characters letters containing only lowercase letters, and given a target letter target, find the smallest element in the list that is larger than the given target.
+
+Letters also wrap around. For example, if the target is target = 'z' and letters = ['a', 'b'], the answer is 'a'.
+
+Examples:
+Input:
+letters = ["c", "f", "j"]
+target = "a"
+Output: "c"
+
+Input:
+letters = ["c", "f", "j"]
+target = "c"
+Output: "f"
+
+Input:
+letters = ["c", "f", "j"]
+target = "d"
+Output: "f"
+
+Input:
+letters = ["c", "f", "j"]
+target = "g"
+Output: "j"
+
+Input:
+letters = ["c", "f", "j"]
+target = "j"
+Output: "c"
+
+Input:
+letters = ["c", "f", "j"]
+target = "k"
+Output: "c"
