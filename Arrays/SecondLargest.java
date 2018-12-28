480da914a7f57fc376dc45bb55bdd5c16a604935
Madhu Priya <madhupriya424@gmail.com>
2018-12-28 11:28:10 +0530

    Update SecondLargest.java

--- Arrays/SecondLargest.java
+++ Arrays/SecondLargest.java
@@ -24,3 +24,39 @@ for(int i = 1; i<arr.length(); i++){
 
 
 time: O(n)
+----------------------------------------------------------------------------------------------------------------------------------
+    
+    /*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+		    int n = sc.nextInt();
+		    int a[] = new int[n];
+		    for(int i=0;i<n;i++){
+		        a[i]=sc.nextInt();
+		    }
+		    
+		    int first = a[0], second = Integer.MIN_VALUE;
+		    for(int i=1;i<n;i++)
+		    {
+		    if(first<a[i]){
+		        second = first;
+		        first = a[i];
+		    }else if(second<a[i])
+		          second = a[i];
+		     }
+		       System.out.println(second);
+		} 
+		 }
+		    
+		   
+		}
+	
