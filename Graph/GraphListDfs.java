da39496aa3e9a15ddc0051dece56248d157b4a81
Madhu Priya <madhupriya424@gmail.com>
2021-01-17 14:21:55 +0530

    Update GraphListDfs.java

--- Graph/GraphListDfs.java
+++ Graph/GraphListDfs.java
@@ -59,3 +59,58 @@ class GraphListDfs
 }
 
 # 0 1 2 3 4 
+	
+//
+
+/*package whatever //do not write package name here */
+
+import java.util.*;
+
+class Graph {
+    // Adjacency matrix
+    private int v;
+    private LinkedList<Integer> adj[];
+    
+    Graph(int v){
+        this.v = v;
+        adj = new LinkedList[v];
+        for(int i = 0; i < v; i++) {
+            adj[i] = new LinkedList<Integer>();
+        }
+    }
+    
+    public void addEdge(int u, int v) {
+        adj[u].add(v);
+    }
+    
+    public void dfs(int src) {
+        boolean visited[] = new boolean[v];
+        // visited[src] = true;
+        dfs(src, visited);
+    }
+    
+    public void dfs(int src, boolean visited[]) {
+        
+        visited[src] = true;
+        System.out.print(src+" ");
+        
+        for(int i = 0; i < adj[src].size(); i++) { 
+            if(!visited[adj[src].get(i)]) {
+                dfs(adj[src].get(i), visited);
+            }
+        }
+    }
+    
+	public static void main (String[] args) {
+	    Graph g = new Graph(4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+        
+        g.dfs(2);
+	}
+}
+	
