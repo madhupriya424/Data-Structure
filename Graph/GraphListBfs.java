47f3257ba3fd9f1a25f86eb504651e8dfe45b8e3
Madhu Priya <madhupriya424@gmail.com>
2021-01-17 15:02:56 +0530

    Update GraphListBfs.java

--- Graph/GraphListBfs.java
+++ Graph/GraphListBfs.java
@@ -69,3 +69,66 @@ class GraphListBfs {
 }
 
 #  0 1 4 2 3 
+
+//
+/*package whatever //do not write package name here */
+
+import java.util.*;
+
+class Graph {
+
+    private int v;
+    private LinkedList<Integer> adj[];
+    
+    Graph (int v) {
+        this.v = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new LinkedList<Integer>();
+        }
+    }
+    
+    public void addEdge(int u, int v) {
+        adj[u].add(v);
+    }
+    
+    public void bfs(int src) {
+        boolean visited[] = new boolean[v];
+        Queue<Integer> q = new LinkedList<Integer>();
+        q.add(src);
+        visited[src] = true;
+        
+        while(!q.isEmpty()) {
+            int curr = q.poll();
+            System.out.print(curr+" ");
+            
+            for(int i = 0; i < adj[curr].size(); i++) {
+                if(!visited[adj[curr].get(i)]) {
+                    q.add(adj[curr].get(i));
+                    visited[adj[curr].get(i)] = true;
+                }
+            }
+            
+            // for(int i : adj[curr]) {
+            //     if(!visited[i]) {
+            //         visited[i] = true;
+            //         q.add(i);
+            //     }
+            // }
+            
+        }
+    }
+    
+	public static void main (String[] args) {
+	    Graph g = new Graph(4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+	
+	    g.bfs(2);
+	}
+}
+// 2 0 3 1
